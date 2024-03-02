a97dfb3329be1c7fad9c9f5cebea5ae29cf2f4b2
miawithcode <cuttontail@gmail.com>
2024-03-03 00:02:13 +0800

    chore: modify hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Your go-to marketplace for unleashing creativity with exceptional{' '}
+        <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
+          Your go-to marketplace for exceptional{' '}
           <span className=" text-blue-600">digital assets</span>.
         </h1>
         <p className="mt-6 text-lg max-w-prose text-muted-foreground">
