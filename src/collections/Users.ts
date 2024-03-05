957d4f041d28fac73465f3fab382327f9c81743f
miawithcode <cuttontail@gmail.com>
2024-03-05 21:10:28 +0800

    feat: setup email send service

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from 'payload/types';
 
 export const Users: CollectionConfig = {
   slug: 'users',
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({token}) => {
+        return `<p>Please verify your email</p>`
+      }
+    }
+  },
   access: {
     read: () => true,
     create: () => true,
--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,9 +2,20 @@ import dotenv from 'dotenv';
 import path from 'path';
 import type { InitOptions } from 'payload/config';
 import payload, { Payload } from 'payload';
+import nodemailer from 'nodemailer';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.resend.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'resend',
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -20,7 +31,9 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payload> => {
+export const getPayloadClient = async ({
+  initOptions,
+}: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error('PAYLOAD_SECRET is missing');
   }
@@ -31,6 +44,11 @@ export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payl
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: 'onboarding@resend.dev', // TODO: change this when deploy
+        fromName: 'DigitalHippo',
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
