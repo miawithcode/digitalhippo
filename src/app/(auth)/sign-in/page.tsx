f84cd8cc98f6d781dc4402d5193e0586ec38bb6b
miawithcode <cuttontail@gmail.com>
2024-03-06 18:02:52 +0800

    fix: refresh to get user state after signed in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -39,7 +39,7 @@ const Page = () => {
   });
 
   const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
-    onSuccess: () => {
+    onSuccess: async () => {
       toast.success('Signed in successfully');
 
       router.refresh();
