414794c33ace7661a3aec3fed35a27f709a50a1e
miawithcode <cuttontail@gmail.com>
2024-03-11 21:31:40 +0800

    chore: update hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
-          Your go-to marketplace for exceptional{' '}
-          <span className=" text-blue-600">digital assets</span>.
+          A curated marketplace for high-quality{' '}
+          <span className=" text-blue-600">digital products</span>.
         </h1>
         <p className="mt-6 text-lg max-w-prose text-muted-foreground">
           Every asset on DigitalHippo is carefully verified by team members,
