f2de36b74b6401d405f46105096770c3d1a0f52f
miawithcode <cuttontail@gmail.com>
2024-03-15 00:28:13 +0800

    feat: design products and media collection schema

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,125 @@
+import { PRODUCT_CATEGORIES } from '../../config';
+import { CollectionConfig } from 'payload/types';
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {},
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'users',
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      label: 'Product Detail',
+      type: 'textarea',
+    },
+    {
+      name: 'price',
+      label: 'Price($)',
+      min: 0,
+      max: 1000,
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'category',
+      label: 'Category',
+      type: 'select',
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product file(s)',
+      type: 'relationship',
+      required: true,
+      relationTo: 'product_files',
+      hasMany: true,
+    },
+    {
+      name: 'approvedForSale',
+      label: 'Product Status',
+      type: 'select',
+      defaultValue: 'pending',
+      access: {
+        create: ({ req }) => req.user.role === 'admin',
+        read: ({ req }) => req.user.role === 'admin',
+        update: ({ req }) => req.user.role === 'admin',
+      },
+      options: [
+        {
+          label: 'Pending verification',
+          value: 'pending',
+        },
+        {
+          label: 'Approved',
+          value: 'approved',
+        },
+        {
+          label: 'Denied',
+          value: 'denied',
+        },
+      ],
+    },
+    {
+      name: 'priceId',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'stripeId',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'images',
+      label: 'Product images',
+      type: 'array',
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: 'Image',
+        plural: 'Images',
+      },
+      fields: [
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+          required: true,
+        },
+      ],
+    },
+  ],
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,10 @@ import { slateEditor } from '@payloadcms/richtext-slate';
 import { buildConfig } from 'payload/config';
 import { webpackBundler } from '@payloadcms/bundler-webpack';
 import path from 'path';
-import { Users } from './collections/Users';
 import dotenv from "dotenv"
+import { Users } from './collections/Users';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Media';
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env")
@@ -12,7 +14,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users],
+  collections: [Users, Products, Media],
   routes: {
     admin: '/sell',
   },
