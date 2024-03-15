1b3a39fb8d767eeb890e9c7be99a2dfa32a2cef5
miawithcode <cuttontail@gmail.com>
2024-03-16 02:40:54 +0800

    style: add stagger animation for render products

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Product } from '@/payload-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ProductSkeleton } from './Skeletons';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 interface ProductListingProps {
   product: Product | null;
@@ -10,11 +12,29 @@ interface ProductListingProps {
 }
 
 const ProductListing = ({ product, index }: ProductListingProps) => {
-
   // for stagger animation
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, index * 75);
+
+    return clearTimeout(timer);
+  }, [index]);
+
   if (!product || !isVisible) return <ProductSkeleton />;
+
+  if (isVisible && product) {
+    return (
+      <Link
+        className={cn('invisible h-full w-full cursor-pointer group/main', {
+          'visible animate-in fade-in-5': isVisible,
+        })}
+        href={`/products/${product.id}`}
+      ></Link>
+    );
+  }
 };
 
 export default ProductListing;
