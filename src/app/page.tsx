430770b666fe23657a11f84321a720f7aa83c54b
miawithcode <cuttontail@gmail.com>
2024-03-16 01:09:32 +0800

    feat(products): create reusable ProductReel component and its title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from '../components/Hero';
 import BenefitsList from '../components/BenefitsList';
+import Products from '../components/Products';
 
 export default function Home() {
   return (
     <>
       <Hero />
 
-      {/* TODO: List products */}
+      <Products />
 
       <BenefitsList />
     </>
---
+++ src/components/Products.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from './MaxWidthWrapper';
+import ProductReel from './ProductReel';
+
+const Products = () => {
+  return (
+    <MaxWidthWrapper>
+      <ProductReel title='Brand new' />
+    </MaxWidthWrapper>
+  );
+};
+export default Products;
