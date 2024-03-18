737864e6e09589344a2d3a3b69221f0c9be0629d
miawithcode <cuttontail@gmail.com>
2024-03-19 00:14:26 +0800

    style: redesign product detail page layout

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import AddToCartButton from '@/components/AddToCartButton';
 import ImageSlider from '@/components/ImageSlider';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import ProductReel from '@/components/ProductReel';
@@ -52,8 +53,8 @@ const Page = async ({ params }: PageProps) => {
     <MaxWidthWrapper className="bg-white">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          {/* BreadCrumbs */}
           <div className="lg:max-w-lg lg:self-end">
+            {/* BreadCrumbs */}
             <ol className="flex items-center space-x-2">
               {BREADCRUMBS.map((breadcrumb, index) => (
                 <li key={breadcrumb.href}>
@@ -79,60 +80,62 @@ const Page = async ({ params }: PageProps) => {
               ))}
             </ol>
 
-            {/* Product Detail */}
+            {/* Product Name */}
             <div className="mt-4">
               <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                 {product.name}
               </h1>
             </div>
-
-            <section className="mt-4">
-              <div className="flex items-center">
-                <p className="font-medium text-gray-900">
-                  {formatPrice(product.price)}
-                </p>
-
-                <div className="ml-4 border-1 text-muted-foreground border-gray-300 pl-4">
-                  {label}
-                </div>
-              </div>
-
-              <div className="mt-4 space-y-6">
-                <p className="text-based text-muted-foreground">
-                  {product.description}
-                </p>
-              </div>
-
-              <div className="mt-6 flex items-center">
-                <Check
-                  aria-hidden="true"
-                  className="h-5 w-5 flex-shrink-0 text-green-500"
-                />
-                <p className="ml-2 text-sm text-muted-foreground">
-                  Eligible for instant delivery
-                </p>
-              </div>
-            </section>
           </div>
 
           {/* Product Images */}
-          <div className="mt-19 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:self-center">
+          <div className=" mt-8 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:self-center">
             <div className="aspect-square rounded-lg">
               <ImageSlider urls={validUrls} />
             </div>
           </div>
 
-          {/* Add To Cart */}
           <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
             <div>
-              <div className="mt-10">Add to cart</div>
-              <div className="mt-6 text-center">
+              {/* Product Detail */}
+              <section className="mt-4">
+                <div className="flex items-center">
+                  <p className="font-medium text-gray-900">
+                    {formatPrice(product.price)}
+                  </p>
+
+                  <div className="ml-4 border-1 text-muted-foreground border-gray-300 pl-4">
+                    {label}
+                  </div>
+                </div>
+
+                <div className="mt-4 space-y-6">
+                  <p className="text-based text-muted-foreground">
+                    {product.description}
+                  </p>
+                </div>
+
+                <div className="mt-6 flex items-center">
+                  <Check
+                    aria-hidden="true"
+                    className="h-5 w-5 flex-shrink-0 text-green-500"
+                  />
+                  <p className="ml-2 text-sm text-muted-foreground">
+                    Eligible for instant delivery
+                  </p>
+                </div>
+              </section>
+              {/* Add To Cart Button */}
+              <div className="mt-10">
+                <AddToCartButton />
+              </div>
+              <div className="mt-6">
                 <div className="group inline-flex text-sm text-medium">
                   <Shield
                     aria-hidden="true"
                     className="mr-2 h-5 w-5 flex-shrink-0 text-gray-400"
                   />
-                  <span className="text-muted-foreground hover:text-gray-300">
+                  <span className="text-muted-foreground hover:text-gray-500">
                     30 Day Return Guarantee
                   </span>
                 </div>
---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,6 @@
+import { Button } from './ui/button';
+
+const AddToCartButton = () => {
+  return <Button size="lg">Add to cart</Button>;
+};
+export default AddToCartButton;
