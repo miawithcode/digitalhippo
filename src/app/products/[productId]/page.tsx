2bfc00308738bd4b6f6c6e2d88f04552edb1489a
miawithcode <cuttontail@gmail.com>
2024-03-19 00:28:41 +0800

    feat: add AddToCart Button instant reaction

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -53,8 +53,8 @@ const Page = async ({ params }: PageProps) => {
     <MaxWidthWrapper className="bg-white">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          {/* BreadCrumbs */}
           <div className="lg:max-w-lg lg:self-end">
-            {/* BreadCrumbs */}
             <ol className="flex items-center space-x-2">
               {BREADCRUMBS.map((breadcrumb, index) => (
                 <li key={breadcrumb.href}>
@@ -79,25 +79,31 @@ const Page = async ({ params }: PageProps) => {
                 </li>
               ))}
             </ol>
-
-            {/* Product Name */}
-            <div className="mt-4">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                {product.name}
-              </h1>
-            </div>
           </div>
 
           {/* Product Images */}
-          <div className=" mt-8 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:self-center">
+          <div className="mt-8 lg:col-start-1 lg:row-start-2 lg:self-center">
             <div className="aspect-square rounded-lg">
               <ImageSlider urls={validUrls} />
             </div>
           </div>
 
-          <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+          <div className="mt-10 lg:mt-4 lg:col-start-2 lg:row-start-2 lg:max-w-lg lg:self-start">
             <div>
               {/* Product Detail */}
+
+              <div className="mt-4">
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  {product.name}
+                </h1>
+              </div>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-based text-muted-foreground">
+                  {product.description}
+                </p>
+              </div>
+
               <section className="mt-4">
                 <div className="flex items-center">
                   <p className="font-medium text-gray-900">
@@ -107,12 +113,6 @@ const Page = async ({ params }: PageProps) => {
                   <div className="ml-4 border-1 text-muted-foreground border-gray-300 pl-4">
                     {label}
                   </div>
-                </div>
-
-                <div className="mt-4 space-y-6">
-                  <p className="text-based text-muted-foreground">
-                    {product.description}
-                  </p>
                 </div>
 
                 <div className="mt-6 flex items-center">
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { Button } from './ui/button';
 
 const AddToCartButton = () => {
-  return <Button size="lg">Add to cart</Button>;
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [isSuccess]);
+
+  return (
+    <Button onClick={() => setIsSuccess(true)} size="lg">
+      {isSuccess ? 'Added!' : 'Add to cart'}
+    </Button>
+  );
 };
 export default AddToCartButton;
