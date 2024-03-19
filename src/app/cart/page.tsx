e5d33b27eace0c1fb004f0266e9a19aa8ce29b11
miawithcode <cuttontail@gmail.com>
2024-03-19 20:11:25 +0800

    feat: display total price and button on checkout page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import { Button } from '@/components/ui/button';
 import { PRODUCT_CATEGORIES } from '@/config';
 import { useCart } from '@/hooks/useCart';
@@ -11,9 +12,16 @@ import Link from 'next/link';
 const Page = () => {
   const { items, removeItem } = useCart();
 
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  );
+
+  const fee = 1;
+
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-5 lg:max-w-7xl lg:px-8">
+    <MaxWidthWrapper className="bg-white">
+      <div className="mx-auto pb-24 pt-16">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Shopping Cart
         </h1>
@@ -126,9 +134,46 @@ const Page = () => {
               })}
             </ul>
           </div>
+
+          <section className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+            <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
+
+            <div className="mt-6 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="text-sm text-gray-600">Subtotal</p>
+                <p className="text-sm font-medium text-gray-900">
+                  {formatPrice(cartTotal)}
+                </p>
+              </div>
+
+              <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                <div className="flex items-center text-sm text-gray-600">
+                  <span>Flat Transaction Fee</span>
+                </div>
+                <div className="text-sm font-medium text-gray-900">
+                  {formatPrice(fee)}
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                <div className="text-base font-medium text-gray-900">
+                  Order Total
+                </div>
+                <div className="text-base font-medium text-gray-900">
+                  {formatPrice(cartTotal + fee)}
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <Button className="w-full" size="lg">
+                Checkout
+              </Button>
+            </div>
+          </section>
         </div>
       </div>
-    </div>
+    </MaxWidthWrapper>
   );
 };
 export default Page;
