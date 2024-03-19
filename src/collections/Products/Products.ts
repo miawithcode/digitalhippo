456e3b3d8f50b2f676cb5a9884567fb407d0629f
miawithcode <cuttontail@gmail.com>
2024-03-20 04:05:40 +0800

    restric user can only read and modify their own product

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -3,8 +3,8 @@ import {
   BeforeChangeHook,
 } from 'payload/dist/collections/config/types';
 import { PRODUCT_CATEGORIES } from '../../config';
-import { CollectionConfig } from 'payload/types';
-import { Product } from '../../payload-types';
+import { Access, CollectionConfig } from 'payload/types';
+import { Product, User } from '../../payload-types';
 import { stripe } from '../../lib/stripe';
 
 const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
@@ -44,12 +44,45 @@ const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
   }
 };
 
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === 'admin') return true;
+
+    const userProductIDs = (user.products || []).reduce<Array<string>>(
+      (acc, product) => {
+        if (!product) return acc;
+        if (typeof product === 'string') {
+          acc.push(product);
+        } else {
+          acc.push(product.id);
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    };
+  };
+
 export const Products: CollectionConfig = {
   slug: 'products',
   admin: {
     useAsTitle: 'name',
   },
-  access: {},
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
     afterChange: [syncUser],
     beforeChange: [
