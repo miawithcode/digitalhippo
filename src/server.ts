ab076e86f4b7f96bdc5a917dc0ac31e622a86fa2
miawithcode <cuttontail@gmail.com>
2024-03-20 02:10:35 +0800

    feat: listen for stripe webhook locally and create receipt email

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webhooks';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -16,9 +19,18 @@ const createContext = ({
   res,
 });
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
 
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
