55140bce7453850a6f1c6c13aeaae55f05dfc710
miawithcode <cuttontail@gmail.com>
2024-03-20 03:33:26 +0800

    feat: update user collection access

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,14 @@
-import { CollectionConfig } from 'payload/types';
+import { Access, CollectionConfig } from 'payload/types';
+
+const adminAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true;
+
+  return {
+    id: {
+      equals: user.id,
+    },
+  };
+};
 
 export const Users: CollectionConfig = {
   slug: 'users',
@@ -10,10 +20,37 @@ export const Users: CollectionConfig = {
     },
   },
   access: {
-    read: () => true,
+    read: adminAndUser,
     create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
   },
   fields: [
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    {
+      name: 'products_files',
+      label: 'Products files',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: true,
+    },
+
     {
       name: 'role',
       defaultValue: 'user',
