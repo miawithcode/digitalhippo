fce16975ae65f730217ec4cb70e2147056d26804
miawithcode <cuttontail@gmail.com>
2024-03-20 04:53:09 +0800

    fix: show image in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: 'localhost',
-        pathname: '**',
-        port: '3000',
-        protocol: 'http',
-      },
+    domains: [
+      'localhost',
+      'digitalhippo-marketplace-production.up.railway.app',
     ],
   },
 };
